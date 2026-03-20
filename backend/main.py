d23afbc3bb155f5eb4e9a22ff56004cb0e5b1bd3
João Miguel <joao.miguel1993@gmail.com>
2026-03-20 12:56:21 +0000

    Update server

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from backend.db import get_connection
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
